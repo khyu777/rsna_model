1b639f6be65a9220f1e2ef4211fab278b92f4887
Peter Yu <yukw777@outlook.com>
2018-10-13 17:57:53 -0400

    train.py done

--- train.py
+++ train.py
@@ -1,223 +1,78 @@
-import os 
-import sys
-import random
-import math
-import numpy as np
-import cv2
-import matplotlib.pyplot as plt
-import json
-import pydicom
-from imgaug import augmenters as iaa
-from tqdm import tqdm
-import pandas as pd 
-import glob
-
-#set filepath
-abspath = os.path.abspath(__file__)
-dname = os.path.dirname(abspath)
-os.chdir(dname)
-
-#input data directory
-DATA_DIR = 'kaggle/input'
-
-# Directory to save logs and trained model
-ROOT_DIR = 'kaggle/working'
-
-# Import Mask RCNN
-sys.path.append(os.path.join(ROOT_DIR, 'Mask_RCNN'))  # To find local version of the library
-from mrcnn.config import Config
-from mrcnn import utils
+import argparse
 import mrcnn.model as modellib
-from mrcnn import visualize
-from mrcnn.model import log
 
-train_dicom_dir = os.path.join(DATA_DIR, 'stage_1_train_images')
-test_dicom_dir = os.path.join(DATA_DIR, 'stage_1_test_images')
-
-def get_dicom_fps(dicom_dir):
-    dicom_fps = glob.glob(dicom_dir+'/'+'*.dcm')
-    return list(set(dicom_fps))
-
-def parse_dataset(dicom_dir, anns): 
-    image_fps = get_dicom_fps(dicom_dir)
-    image_annotations = {fp: [] for fp in image_fps}
-    for index, row in anns.iterrows(): 
-        fp = os.path.join(dicom_dir, row['patientId']+'.dcm')
-        image_annotations[fp].append(row)
-    return image_fps, image_annotations
-
-class DetectorConfig(Config):
-    """Configuration for training pneumonia detection on the RSNA pneumonia dataset.
-    Overrides values in the base Config class.
-    """
-    
-    # Give the configuration a recognizable name  
-    NAME = 'pneumonia'
-    
-    # Train on 1 GPU and 8 images per GPU. We can put multiple images on each
-    # GPU because the images are small. Batch size is 8 (GPUs * images/GPU).
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 4 
-    
-    BACKBONE = 'resnet50'
-    
-    NUM_CLASSES = 2  # background + 1 pneumonia classes
-
-    '''
-    IMAGE_MIN_DIM = 256
-    IMAGE_MAX_DIM = 256
-    RPN_ANCHOR_SCALES = (32, 64, 128, 256)
-    TRAIN_ROIS_PER_IMAGE = 32
-    MAX_GT_INSTANCES = 3
-    DETECTION_MAX_INSTANCES = 3
-    DETECTION_MIN_CONFIDENCE = 0.9
-    DETECTION_NMS_THRESHOLD = 0.1
-
-    STEPS_PER_EPOCH = 100
-    '''
-    
-config = DetectorConfig()
-
-class DetectorDataset(utils.Dataset):
-    """Dataset class for training pneumonia detection on the RSNA pneumonia dataset.
-    """
-
-    def __init__(self, image_fps, image_annotations, orig_height, orig_width):
-        super().__init__(self)
-        
-        # Add classes
-        self.add_class('pneumonia', 1, 'Lung Opacity')
-   
-        # add images 
-        for i, fp in enumerate(image_fps):
-            annotations = image_annotations[fp]
-            self.add_image('pneumonia', image_id=i, path=fp, 
-                           annotations=annotations, orig_height=orig_height, orig_width=orig_width)
-            
-    def image_reference(self, image_id):
-        info = self.image_info[image_id]
-        return info['path']
-
-    def load_image(self, image_id):
-        info = self.image_info[image_id]
-        fp = info['path']
-        ds = pydicom.read_file(fp)
-        image = ds.pixel_array
-        # If grayscale. Convert to RGB for consistency.
-        if len(image.shape) != 3 or image.shape[2] != 3:
-            image = np.stack((image,) * 3, -1)
-        return image
-
-    def load_mask(self, image_id):
-        info = self.image_info[image_id]
-        annotations = info['annotations']
-        count = len(annotations)
-        if count == 0:
-            mask = np.zeros((info['orig_height'], info['orig_width'], 1), dtype=np.uint8)
-            class_ids = np.zeros((1,), dtype=np.int32)
-        else:
-            mask = np.zeros((info['orig_height'], info['orig_width'], count), dtype=np.uint8)
-            class_ids = np.zeros((count,), dtype=np.int32)
-            for i, a in enumerate(annotations):
-                if a['Target'] == 1:
-                    x = int(a['x'])
-                    y = int(a['y'])
-                    w = int(a['width'])
-                    h = int(a['height'])
-                    mask_instance = mask[:, :, i].copy()
-                    cv2.rectangle(mask_instance, (x, y), (x+w, y+h), 255, -1)
-                    mask[:, :, i] = mask_instance
-                    class_ids[i] = 1
-        return mask.astype(np.bool), class_ids.astype(np.int32)
-
-anns = pd.read_csv(os.path.join(DATA_DIR, 'stage_1_train_labels.csv'))
-
-image_fps, image_annotations = parse_dataset(train_dicom_dir, anns=anns)
-
-ds = pydicom.read_file(image_fps[0]) # read dicom image from filepath 
-image = ds.pixel_array # get image array
-
-ORIG_SIZE = 1024
-
-######################################################################
-# Modify this line to use more or fewer images for training/validation. 
-# To use all images, do: image_fps_list = list(image_fps)
-image_fps_list = list(image_fps[:1000]) 
-#####################################################################
-
-# split dataset into training vs. validation dataset 
-# split ratio is set to 0.9 vs. 0.1 (train vs. validation, respectively)
-sorted(image_fps_list)
-random.seed(42)
-random.shuffle(image_fps_list)
-
-validation_split = 0.1
-split_index = int((1 - validation_split) * len(image_fps_list))
-
-image_fps_train = image_fps_list[:split_index]
-image_fps_val = image_fps_list[split_index:]
-
-print(len(image_fps_train), len(image_fps_val))
-
-# prepare the training dataset
-dataset_train = DetectorDataset(image_fps_train, image_annotations, ORIG_SIZE, ORIG_SIZE)
-dataset_train.prepare()
-
-# Show annotation(s) for a DICOM image 
-test_fp = random.choice(image_fps_train)
-image_annotations[test_fp]
-
-# prepare the validation dataset
-dataset_val = DetectorDataset(image_fps_val, image_annotations, ORIG_SIZE, ORIG_SIZE)
-dataset_val.prepare()
-
-# Load and display random samples and their bounding boxes
-# Suggestion: Run this a few times to see different examples. 
-
-image_id = random.choice(dataset_train.image_ids)
-image_fp = dataset_train.image_reference(image_id)
-image = dataset_train.load_image(image_id)
-mask, class_ids = dataset_train.load_mask(image_id)
-
-print(image.shape)
-
-plt.figure(figsize=(10, 10))
-plt.subplot(1, 2, 1)
-plt.imshow(image[:, :, 0], cmap='gray')
-plt.axis('off')
-
-plt.subplot(1, 2, 2)
-masked = np.zeros(image.shape[:2])
-for i in range(mask.shape[2]):
-    masked += image[:, :, 0] * mask[:, :, i]
-plt.imshow(masked, cmap='gray')
-plt.axis('off')
-
-print(image_fp)
-print(class_ids)
+from imgaug import augmenters as iaa
+from config import from_config_file
+from dataset import get_train_and_val
 
 if __name__ == '__main__':
-    model = modellib.MaskRCNN(mode='training', config=config, model_dir=ROOT_DIR)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', help='path to train config file')
+    parser.add_argument('dicom_dir', help='path to directory with train dicom files')
+    parser.add_argument('label_file', help='path to label file')
+    parser.add_argument('model_dir', help='directory for logs and trained models')
+    parser.add_argument('--coco', help='path to pretrained coco weights')
+    parser.add_argument('--checkpoint', help='path to checkpoint weights')
+    parser.add_argument('--head-epochs', help='number of epochs for head training. Default 2', type=int, default=2)
+    parser.add_argument('--all-epochs', help='list of number of epochs for all layer training. Default 6 16', nargs='+', type=int, default=[6, 16])
+    args = parser.parse_args()
 
-    # Image augmentation 
-    augmentation = iaa.SomeOf((0, 1), [
-        iaa.Fliplr(0.5),
-        iaa.Affine(
-            scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
-            translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
-            rotate=(-25, 25),
-            shear=(-8, 8)
-        ),
-        iaa.Multiply((0.9, 1.1))
+    config = from_config_file(args.config)
+    model = modellib.MaskRCNN(mode='training', config=config, model_dir=args.model_dir)
+    if args.coco:
+        model.load_weights(args.coco, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask",
+        ])
+    elif args.checkpoint:
+        model.load_weights(args.checkpoint)
+
+    # Image augmentation
+    augmentation = iaa.Sequential([
+        iaa.OneOf([ ## geometric transform
+            iaa.Affine(
+                scale={"x": (0.98, 1.02), "y": (0.98, 1.04)},
+                translate_percent={"x": (-0.02, 0.02), "y": (-0.04, 0.04)},
+                rotate=(-2, 2),
+                shear=(-1, 1),
+            ),
+            iaa.PiecewiseAffine(scale=(0.001, 0.025)),
+        ]),
+        iaa.OneOf([ ## brightness or contrast
+            iaa.Multiply((0.9, 1.1)),
+            iaa.ContrastNormalization((0.9, 1.1)),
+        ]),
+        iaa.OneOf([ ## blur or sharpen
+            iaa.GaussianBlur(sigma=(0.0, 0.1)),
+            iaa.Sharpen(alpha=(0.0, 0.1)),
+        ]),
     ])
 
-    NUM_EPOCHS = 1
+    # train and validation sets
+    dataset_train, dataset_val = get_train_and_val(args.dicom_dir, args.label_file)
 
-    # Train Mask-RCNN Model 
-    import warnings 
+    # Train Mask-RCNN Model
+    import warnings
     warnings.filterwarnings("ignore")
-    model.train(dataset_train, dataset_val, 
-                learning_rate=config.LEARNING_RATE, 
-                epochs=NUM_EPOCHS, 
+
+    print('Train network heads')
+    lr = config.LEARNING_RATE*2
+    print('Learning rate %f' % lr)
+    # Train the heads with higher learning rate first
+    model.train(dataset_train, dataset_val,
+                learning_rate=lr,
+                epochs=args.head_epochs,
+                layers='heads',
+                augmentation=augmentation)
+
+    print('Train all layers')
+    lr = config.LEARNING_RATE
+    for i, epoch in enumerate(args.all_epochs):
+        print('Stage %d' % i+1)
+        lr /= 5
+        print('Learning rate %f' % lr)
+        model.train(dataset_train, dataset_val,
+                learning_rate=lr,
+                epochs=epoch,
                 layers='all',
                 augmentation=augmentation)
