9972428a6070e9715dbdfc56811bafeea36de726
Peter Yu <yukw777@outlook.com>
2018-10-14 15:49:15 -0400

    correctly accept additional options in train.sh

--- train.sh
+++ train.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ "$#" -ne 6 ]; then
+if [ "$#" -lt 5 ]; then
     echo "Usage: $0 <data-dir> <config-file> <results-dir> <weights-dir> <available-gpus> <options>"
     exit 1
 fi
@@ -8,4 +8,4 @@ docker run -e "NVIDIA_VISIBLE_DEVICES=$5" --runtime=nvidia --rm \
     -v `pwd`/`dirname $2`:/config \
     -v `pwd`/"$3":/results \
     -v `pwd`/"$4":/weights \
-    yukw777/rsna-model python train.py /config/`basename $2` /data/stage_1_train_images /data/stage_1_train_labels.csv /results "$6"
+    yukw777/rsna-model python train.py /config/`basename $2` /data/stage_1_train_images /data/stage_1_train_labels.csv /results "${@:6}"
