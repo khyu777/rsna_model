a9f21906b5bec711c03f17648d6283dff5c7ac35
Peter Yu <yukw777@outlook.com>
2018-10-15 22:00:23 -0400

    eval.py can produce submission.csv

--- eval.py
+++ eval.py
@@ -1,4 +1,10 @@
-import os 
+import argparse
+import os
+import mrcnn.model as modellib
+
+from mrcnn import utils
+from config import from_config_file
+
 import sys
 import random
 import math
@@ -9,154 +15,87 @@ import json
 import pydicom
 from imgaug import augmenters as iaa
 from tqdm import tqdm
-import pandas as pd 
+import pandas as pd
 import glob
 import train as tr
 
-# Import Mask RCNN
-sys.path.append(os.path.join(tr.ROOT_DIR, 'Mask_RCNN'))  # To find local version of the library
-from mrcnn.config import Config
-from mrcnn import utils
-import mrcnn.model as modellib
-from mrcnn import visualize
-from mrcnn.model import log
 
-# select trained model 
-dir_names = next(os.walk(tr.ROOT_DIR))[1]
-key = tr.config.NAME.lower()
-dir_names = filter(lambda f: f.startswith(key), dir_names)
-dir_names = sorted(dir_names)
-
-if not dir_names:
-    import errno
-    raise FileNotFoundError(
-        errno.ENOENT,
-        "Could not find model directory under {}".format(self.model_dir))
-
-fps = []
-# Pick last directory
-for d in dir_names: 
-    dir_name = os.path.join(tr.ROOT_DIR, d)
-    # Find the last checkpoint
-    checkpoints = next(os.walk(dir_name))[2]
-    checkpoints = filter(lambda f: f.startswith("mask_rcnn"), checkpoints)
-    checkpoints = sorted(checkpoints)
-    if not checkpoints:
-        print('No weight files in {}'.format(dir_name))
-    else: 
-      
-      checkpoint = os.path.join(dir_name, checkpoints[-1])
-      fps.append(checkpoint)
-
-model_path = sorted(fps)[-1]
-print('Found model {}'.format(model_path))
-
-class InferenceConfig(tr.DetectorConfig):
-    GPU_COUNT = 1
-    IMAGES_PER_GPU = 1
-
-inference_config = InferenceConfig()
-
-# Recreate the model in inference mode
-model = modellib.MaskRCNN(mode='inference', 
-                          config=inference_config,
-                          model_dir=tr.ROOT_DIR)
-
-# Load trained weights (fill in path to trained weights here)
-assert model_path != "", "Provide path to trained weights"
-print("Loading weights from ", model_path)
-model.load_weights(model_path, by_name=True)
-
-# set color for class
-def get_colors_for_class_ids(class_ids):
-    colors = []
-    for class_id in class_ids:
-        if class_id == 1:
-            colors.append((.941, .204, .204))
-    return colors
-
-# Show few example of ground truth vs. predictions on the validation dataset 
-dataset = tr.dataset_val
-fig = plt.figure(figsize=(10, 30))
-
-for i in range(4):
-
-    image_id = random.choice(dataset.image_ids)
-    
-    original_image, image_meta, gt_class_id, gt_bbox, gt_mask =\
-        modellib.load_image_gt(tr.dataset_val, inference_config, 
-                               image_id, use_mini_mask=False)
-    
-    print(original_image.shape)
-    plt.subplot(6, 2, 2*i + 1)
-    visualize.display_instances(original_image, gt_bbox, gt_mask, gt_class_id, 
-                                dataset.class_names,
-                                colors=get_colors_for_class_ids(gt_class_id), ax=fig.axes[-1])
-    
-    plt.subplot(6, 2, 2*i + 2)
-    results = model.detect([original_image]) #, verbose=1)
-    r = results[0]
-    visualize.display_instances(original_image, r['rois'], r['masks'], r['class_ids'], 
-                                dataset.class_names, r['scores'], 
-                                colors=get_colors_for_class_ids(r['class_ids']), ax=fig.axes[-1])
-
-# Get filenames of test dataset DICOM images
-test_image_fps = tr.get_dicom_fps(tr.test_dicom_dir)
-
-# Make predictions on test images, write out sample submission 
-def predict(image_fps, filepath='submission.csv', min_conf=0.95): 
-    
-    # assume square image
-    #resize_factor = ORIG_SIZE / config.IMAGE_SHAPE[0]
-    resize_factor = tr.ORIG_SIZE 
+# Make predictions on test images, write out sample submission
+def predict(image_fps, config, min_conf, filepath):
     with open(filepath, 'w') as file:
-      for image_id in tqdm(image_fps): 
+      for image_id in tqdm(image_fps):
         ds = pydicom.read_file(image_id)
         image = ds.pixel_array
-        # If grayscale. Convert to RGB for consistency.
-        if len(image.shape) != 3 or image.shape[2] != 3:
-            image = np.stack((image,) * 3, -1) 
-        image, window, scale, padding, crop = utils.resize_image(
+        resize_factor = image.shape[0]
+
+        # Convert to RGB for consistency.
+        image = np.stack((image,) * 3, -1)
+        image, _, _, _, _ = utils.resize_image(
             image,
-            min_dim=tr.config.IMAGE_MIN_DIM,
-            min_scale=tr.config.IMAGE_MIN_SCALE,
-            max_dim=tr.config.IMAGE_MAX_DIM,
-            mode=tr.config.IMAGE_RESIZE_MODE)
-            
+            min_dim=config.IMAGE_MIN_DIM,
+            min_scale=config.IMAGE_MIN_SCALE,
+            max_dim=config.IMAGE_MAX_DIM,
+            mode=config.IMAGE_RESIZE_MODE)
+
         patient_id = os.path.splitext(os.path.basename(image_id))[0]
 
         results = model.detect([image])
         r = results[0]
 
         out_str = ""
-        out_str += patient_id 
+        out_str += patient_id
         out_str += ","
         assert( len(r['rois']) == len(r['class_ids']) == len(r['scores']) )
-        if len(r['rois']) == 0: 
+        if len(r['rois']) == 0:
             pass
-        else: 
+        else:
             num_instances = len(r['rois'])
-  
-            for i in range(num_instances): 
-                if r['scores'][i] > min_conf: 
+
+            for i in range(num_instances):
+                if r['scores'][i] > min_conf:
                     out_str += ' '
                     out_str += str(round(r['scores'][i], 2))
                     out_str += ' '
 
-                    # x1, y1, width, height 
+                    # x1, y1, width, height
                     x1 = r['rois'][i][1]
                     y1 = r['rois'][i][0]
-                    width = r['rois'][i][3] - x1 
-                    height = r['rois'][i][2] - y1 
+                    width = r['rois'][i][3] - x1
+                    height = r['rois'][i][2] - y1
                     bboxes_str = "{} {} {} {}".format(x1*resize_factor, y1*resize_factor, \
-                                                       width*resize_factor, height*resize_factor)   
-#                     bboxes_str = "{} {} {} {}".format(x1, y1, \
-#                                                       width, height)
+                                                       width*resize_factor, height*resize_factor)
                     out_str += bboxes_str
 
         file.write(out_str+"\n")
 
-# predict only the first 50 entries
-submission_fp = os.path.join(tr.ROOT_DIR, 'submission.csv')
-print(submission_fp)
-predict(test_image_fps, filepath=submission_fp)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('config', help='path to train config file')
+    parser.add_argument('threshold', help='min threshold for detected boxes', type=float)
+    parser.add_argument('dicom_dir', help='path to directory with eval dicom files')
+    parser.add_argument('model', help='path to the pretrained model')
+    parser.add_argument('eval_dir', help='path to directory with evaluated images')
+    parser.add_argument('--label-file', help='path to label file')
+    args = parser.parse_args()
+
+    # Set inference config
+    config = from_config_file(args.config)
+    config.GPU_COUNT = 1
+    config.IMAGES_PER_GPU = 1
+    config.__init__()
+
+    # Recreate the model in inference mode
+    print('Loading weights from ' + args.model)
+    model = modellib.MaskRCNN(
+        mode='inference',
+        config=config,
+        model_dir=os.path.dirname(args.model)
+    )
+    model.load_weights(args.model, by_name=True)
+    print('Weights loaded')
+
+    # Get filenames of test dataset DICOM images
+    test_image_fps = glob.glob(os.path.join(args.dicom_dir, '*.dcm'))
+
+    predict(test_image_fps, config, args.threshold, args.label_file)
